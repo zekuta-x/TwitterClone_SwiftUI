7723727728bf52dc8c29bc4a631cba328f51f980
Toriyama Hidetaka <mgslove11zekuta@gmail.com>
2022-02-16 17:00:17 +0900

    [fix]Login View

--- TwitterSwiftUI/TwitterSwiftUI/Utils/Views/CustomSecureField.swift
+++ TwitterSwiftUI/TwitterSwiftUI/Utils/Views/CustomSecureField.swift
@@ -1,16 +1,15 @@
 //
-//  CustomtextField.swift
+//  CustomSecureField.swift
 //  TwitterSwiftUI
 //
-//  Created by 鳥山英峻 on 2022/02/10.
+//  Created by 鳥山英峻 on 2022/02/16.
 //
 
 import SwiftUI
 
-struct CustomTextField: View {
+struct CustomSecureField: View {
     @Binding var text: String
     let placeholder: Text
-    let imageName: String
     
     var body: some View {
         ZStack(alignment: .leading) {
@@ -21,13 +20,13 @@ struct CustomTextField: View {
             }
             
             HStack(spacing: 16){
-                Image(systemName:  imageName)
+                Image(systemName:  "lock")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 20, height: 20)
                     .foregroundColor(.white)
                 
-                TextField("", text: $text)
+                SecureField("", text: $text)
             }
         }
     }
