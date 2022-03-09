938349694bc0d0ee3737534be5f6c357c3931f8b
Toriyama Hidetaka <mgslove11zekuta@gmail.com>
2022-03-09 16:53:55 +0900

    [add]root user profile

--- TwitterSwiftUI/TwitterSwiftUI/Model/User.swift
+++ TwitterSwiftUI/TwitterSwiftUI/Model/User.swift
@@ -5,7 +5,7 @@
 //  Created by 鳥山英峻 on 2022/02/25.
 //
 
-import Foundation
+import Firebase
 
 struct User: Identifiable{
     let id: String
@@ -13,6 +13,8 @@ struct User: Identifiable{
     let ProfileImageurl: String
     let fullname: String
     let email: String
+    
+    var isCurrentUser: Bool { return Auth.auth().currentUser?.uid == self.id }
     
     init(dictionary: [String: Any]){
         self.id = dictionary["uid"] as? String ?? ""
